e584582e42327fdb0dcdf03b203b2b2f81efa6be
Sam822008 <72604767+Sam822008@users.noreply.github.com>
2020-11-16 11:50:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,23 +4,20 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var paper;
-var ground;
-
 function preload()
 {
 	
 }
 
 function setup() {
-	var canvas = createCanvas(1200,400);
+	createCanvas(800, 700);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	 ground = new Ground(600,height,1200,20);
-    paper = new Paper(700,320,70,70);
+	//Create the Bodies Here.
+
 
 	Engine.run(engine);
   
@@ -30,10 +27,8 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  Engine.update(engine);
+  
   drawSprites();
-  paper.display();
-  ground.display();
  
 }
 
